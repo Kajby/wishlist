92977cec7a3595a63fcbebd9a105dfd90991d2db
hagy <hagy@list.ru>
2025-04-04 11:58:29 +0300

    My second commit

---
+++ src/main/java/com/example/WishList/WishListApplication.java
@@ -0,0 +1,26 @@
+package com.example.WishList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WishListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishListApplication.class, args);
+
+		st();
+	}
+
+
+	public static void st() {
+
+	}
+
+
+}
+
+
+
+
